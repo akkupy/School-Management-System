3df6b5bd462a67416a9109e1bb31463bfbbc001b
Akash S <akashsureshmyd@gmail.com>
2021-07-01 01:13:59 +0530

    Organising Code :100:

---
+++ Modules/assets.py
@@ -0,0 +1,13 @@
+# Def1:SPACE
+def Enter():
+    print()
+
+
+# Def2:HORIZONTAL LINE
+def Star():
+    print('=' * 121)
+
+
+# Def21:ENTER KEY FUNCTION
+def Lag():
+    input("Press ENTER KEY TO CONTINUE")
